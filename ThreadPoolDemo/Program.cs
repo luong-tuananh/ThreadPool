3dda6bad3970f5a47ccf4ef85dbc4473257df444
RICO\luong <luonganh@gmail.com>
2021-08-03 12:08:14 +0700

    Compare performance of Thread and ThreadPool

--- ThreadPoolDemo/Program.cs
+++ ThreadPoolDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading;
 
 namespace ThreadPoolDemo
@@ -9,17 +10,44 @@ namespace ThreadPoolDemo
         {
             for (int i = 0; i < 10; i++)
             {
-                ThreadPool.QueueUserWorkItem(new WaitCallback(MyMethod));
+                MethodWithThread();
+                MethodWithThreadPool();
             }
+            Stopwatch stopwatch = new Stopwatch();
+            Console.WriteLine("Execution using Thread");
+            stopwatch.Start();
+            MethodWithThread();
+            stopwatch.Stop();
+            Console.WriteLine("Time consumed by MethodWithThread is : " +
+                                 stopwatch.ElapsedTicks.ToString());
+
+            stopwatch.Reset();
+            Console.WriteLine("Execution using Thread Pool");
+            stopwatch.Start();
+            MethodWithThreadPool();
+            stopwatch.Stop();
+            Console.WriteLine("Time consumed by MethodWithThreadPool is : " +
+                                 stopwatch.ElapsedTicks.ToString());
 
             Console.Read();
         }
 
-        public static void MyMethod(object obj)
+        public static void MethodWithThread()
         {
-            Thread thread = Thread.CurrentThread;
-            string message = $"Background: {thread.IsBackground}, Thread Pool: {thread.IsThreadPoolThread}, Thread ID: {thread.ManagedThreadId}";
-            Console.WriteLine(message);
+            for (int i = 0; i < 10; i++)
+            {
+                Thread thread = new Thread(Test);
+            }
+        }
+        public static void MethodWithThreadPool()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                ThreadPool.QueueUserWorkItem(new WaitCallback(Test));
+            }
+        }
+        public static void Test(object obj)
+        {
         }
     }
 }
